317a7b300ebc8674cfbd5eb97c8c0a732f213d14
surendra2522 <yourmail@gmail.com>
2026-05-19 10:37:37 +0530

    Update live city feed

--- backend/routes/analyticsRoutes.js
+++ backend/routes/analyticsRoutes.js
@@ -1,9 +1,8 @@
 import express from 'express'
-import { protect } from '../middleware/authMiddleware.js'
 import { getSummary } from '../controllers/analyticsController.js'
 
 const router = express.Router()
 
-router.get('/summary', protect, getSummary)
+router.get('/summary', getSummary)
 
 export default router
